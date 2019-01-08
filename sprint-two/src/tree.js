998a56b16491a4fd8db8ed95800a08a2ab6ec5d3
alletsz <stellazg@gmail.com>
2019-01-07 20:59:53 -0800

    Provide a working solution for trees

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,44 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  
+
+  //every child is its own tree
+  //create a new tree
+  // add that child tree to root
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+//return boolean reflecting if target is found in target node or any other descendant node
+
+//check value
+//if no => check children
+//use recursion on children
+//base case is if target is found or there is no more children 
+  var found = false; 
+  var traverse = function (node) {
+    if (node.value === target) {
+      found = true;
+    } else if (node.children.length === 0) {
+      return;
+    } else {
+      for (var i = 0; i < node.children.length; i++) {
+        traverse(node.children[i]);
+      }
+    }
+  };
+  traverse(this);
+  return found;
 };
 
 
