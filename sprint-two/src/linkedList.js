782e533df5ba6cc7c30cd050ded5f65ce8a1e5bf
alletsz <stellazg@gmail.com>
2019-01-07 18:29:10 -0800

    Provide a working solution for linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,53 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+  //create a new node
+  // adjust the tail node next reference to refer to the new node
+  // adjust the tail node to the new node
+    var newNode = Node(value);
+    var currNode = list.tail;
+    
+    if (list.tail === null) {
+      list.tail = newNode;
+      list.head = newNode;
+    } else {
+      var previousNode = list.tail;
+      list.tail.next = newNode; 
+      list.tail = newNode;
+    } 
+
   };
 
   list.removeHead = function() {
+    // move head to the next node 
+    //reassign head to next node
+    // return removed value
+    var currHead = list.head.value; 
+    list.head = list.head.next;
+    return currHead;
+
   };
 
   list.contains = function(target) {
+  //return a booloean if target is in linked list
+    
+    var results;
+
+    var traverse = function (node) {
+      if (node.value === target) {
+        results = true;
+      } else if (node.next === null) {
+        results = false;
+      } else {
+        traverse(node.next);
+      }
+      //   traverse(node.next);
+      // }
+    
+    };
+
+    traverse(list.head);
+    return results;
   };
 
   return list;
