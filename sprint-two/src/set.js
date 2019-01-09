12cd453b0498eaf30e65b70d3219fdd6ec72cb83
alletsz <stellazg@gmail.com>
2019-01-08 18:00:03 -0800

    Provide working solution for sets

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,36 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+
+  this.storage.push(item);
 };
 
 setPrototype.contains = function(item) {
+  var currentSet = this.storage;
+  var containedItem = false;
+  for (var i = 0; i < currentSet.length; i++) {
+    if (currentSet[i] === item) {
+      containedItem = true;
+      return containedItem;
+    } 
+  }
+  return containedItem;
 };
 
 setPrototype.remove = function(item) {
+
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      this.storage.splice(i, 1);
+    }
+  }
+  
 };
 
 /*
